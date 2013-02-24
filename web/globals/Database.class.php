c8a51da04cc90f0341392d9e6d0dac59d0d60794
rubenbert <treburp@yahoo.com>
2013-02-24 11:08:05 +0800

    Database class edited

--- web/globals/Database.class.php
+++ web/globals/Database.class.php
@@ -1,12 +1,12 @@
 <?php
 	Class Database extends PDO(){
+		private $host	=	'localhost';
+		private $dbname	=	'poll';
+		private	$dbuser	=	'';
+		private	$dbpass	=	'';
 		public function __construct(){
-			private $host	=	'localhost';
-			private $dbname	=	'poll';
-			private	$dbuser	=	'';
-			private	$dbpass	=	'';
-			private $dsn	=	"mysql:host=$this->host; dbname=$this->dbname";
-			parent::__construct($dsn,$this->dbname,$this->dbpass);
+			$this->$dsn	=	"mysql:host=".$this->host."; dbname=".$this->dbname."";
+			parent::__construct($this->dsn,$this->dbname,$this->dbpass);
 			try{
 				$this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			}Catch(PDOException $e){
