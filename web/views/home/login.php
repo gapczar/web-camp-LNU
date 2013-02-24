27dceab0fdc7a3eb74ba2bc0330e415b9231dae4
rubenbert <treburp@yahoo.com>
2013-02-24 14:16:05 +0800

    views files

---
+++ web/views/home/login.php
@@ -0,0 +1,35 @@
+
+<div class="navbar">
+	<div class="navbar-inner">
+		<a href="" class="brand">LNU</a>
+		<ul class="nav">
+			<li class="active"><a href="/">Home</a></li>
+			<li><a href="applist.php">App list</a></li>
+		</ul>
+	</div>
+</div><!--end of navbar-->
+<div id="body" class="div-padding div-padding">
+	<div class="row-fluid">
+		<div class="span5">
+			<div></div>
+			<div class=" span12 white-bg div-padding glow-effect-shadow round-edges" style="margin-bottom:20px;">
+				<h4>Sign In</h4><hr />
+				<form action="" method="post" onsubmit="return login(this,event);">
+					<table style="width:100%">
+						<tr><td>Username: </td><td><input type='text' name="uname" /></td></tr>
+						<tr><td>Password: </td><td><input type='password' name="upass" /></td></tr>
+						<tr><td colspan=2 align=center><button type="submit" name="is_login" value="true" class="btn btn-primary"><i class="icon-user icon-white"></i> Sign In</button></td></tr>
+					</table>
+				</form>
+			</div>
+		</div>
+		<div class="span7 white-bg div-padding glow-effect-shadow round-edges">
+			<h4><i class="icon-star"></i> About this site</h4><hr />
+			<p style="text-algin:justify">
+				sfsfsdfdsfdsf sfsfsdfdsfdsf sfsfsdfdsfdsf sfsfsdfdsfdsf sfsfsdfdsfdsf sfsfsdfd sfdsf sfsfsdfdsfdsf sfsfsdfdsfdsf sfsfs dfdsfdsf 
+				sfsfsd fdsfdsf sfsfsdfdsfdsf sfsfsdfdsfdsf sfsfsdf dsfdsf sfsfsdfd sfdsf sfsfsdfdsfdsf sfsfsd fdsfdsf sfsfsd fdsfdsf sfsfsd fdsfdsf 
+				sfsfsd fdsfdsf sfsfsdfdsfdsf sfsfsdf dsfdsf sfsfsdfdsfdsf sfsfsd fdsfdsf sfsfs dfdsfdsf sfsfsdfdsfdsf sfsfsdfdsfdsf 
+			</p>
+		</div>
+	</div>
+</div>
---
+++ web/views/home/register.php
@@ -0,0 +1,36 @@
+<div class="navbar">
+	<div class="navbar-inner">
+		<a href="" class="brand">LNU</a>
+		<ul class="nav">
+			<li class="active"><a href="/">Home</a></li>
+			<li><a href="applist.php">App list</a></li>
+		</ul>
+	</div>
+</div><!--end of navbar-->
+<div id="body" class="div-padding div-padding">
+	<div class="row-fluid">
+		<div class="span5">
+			<div class="span12 white-bg div-padding glow-effect-shadow round-edges">
+				<h4>Regsiter</h4><hr />
+				<form>
+					<table style="width:100%">
+						<tr><td>Email Address: </td><td><input type='text' /></td></tr>
+						<tr><td>Username: </td><td><input type='text' /></td></tr>
+						<tr><td>Password: </td><td><input type='password' /></td></tr>
+						<tr><td>Confirm Password: </td><td><input type='password' /></td></tr>
+						<tr><td colspan=2 align=center><button class="btn btn-success"><i class="icon-user icon-white"></i> Regsiter</button> <input type='reset' class="btn" value="Reset" /></td></tr>
+					</table>
+				</form>
+			</div>	
+		</div>
+		
+		<div class="span7 white-bg div-padding glow-effect-shadow round-edges">
+			<h4><i class="icon-star"></i> About this site</h4><hr />
+			<p style="text-algin:justify">
+				sfsfsdfdsfdsf sfsfsdfdsfdsf sfsfsdfdsfdsf sfsfsdfdsfdsf sfsfsdfdsfdsf sfsfsdfd sfdsf sfsfsdfdsfdsf sfsfsdfdsfdsf sfsfs dfdsfdsf 
+				sfsfsd fdsfdsf sfsfsdfdsfdsf sfsfsdfdsfdsf sfsfsdf dsfdsf sfsfsdfd sfdsf sfsfsdfdsfdsf sfsfsd fdsfdsf sfsfsd fdsfdsf sfsfsd fdsfdsf 
+				sfsfsd fdsfdsf sfsfsdfdsfdsf sfsfsdf dsfdsf sfsfsdfdsfdsf sfsfsd fdsfdsf sfsfs dfdsfdsf sfsfsdfdsfdsf sfsfsdfdsfdsf 
+			</p>
+		</div>
+	</div>
+</div>
